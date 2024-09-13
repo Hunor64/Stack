5ac157a08fe20d41bca0970dd82f223211351055
hunor64 <nhunor06@gmail.com>
2024-09-13 08:13:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Stack<int> stack = new();
+Random rand = new();
+for (int i = 0; i < 25; i++)
+{
+    stack.Push(rand.Next(1, 100));
+}
+
+List<int> sortedList = stack.ToList();
+sortedList.Sort();
+sortedList.Reverse();
+stack = new Stack<int>(sortedList);
+
+foreach (var elem in stack)
+{
+    Console.WriteLine(elem);
+}
